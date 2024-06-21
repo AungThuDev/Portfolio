a2abb34b67ddfccb84f6eebce9bc71dd1552943b
AungThuDev <htutthu1991@gmail.com>
2024-06-21 15:42:03 +0630

    adding animateCSS & scriptJS file

---
+++ script.js
@@ -0,0 +1,118 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const box = document.querySelector('.title-head');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate__animated', 'animate__bounce');
+                entry.target.style.opacity = 1;
+                // Stop observing after the element is animated
+                observer.unobserve(entry.target);
+            }
+        });
+    });
+
+    observer.observe(box);
+});
+document.addEventListener('DOMContentLoaded', (event) => {
+    const box = document.querySelector('.sub-head');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate__animated', 'animate__bounce');
+                entry.target.style.opacity = 1;
+                // Stop observing after the element is animated
+                observer.unobserve(entry.target);
+            }
+        });
+    });
+
+    observer.observe(box);
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const box = document.querySelector('.about-image');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate__animated', 'animate__fadeInLeftBig');
+                entry.target.style.opacity = 1;
+                // Stop observing after the element is animated
+                observer.unobserve(entry.target);
+            }
+        });
+    });
+
+    observer.observe(box);
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const box = document.querySelector('.para-div');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate__animated', 'animate__fadeInRightBig');
+                entry.target.style.opacity = 1;
+                // Stop observing after the element is animated
+                observer.unobserve(entry.target);
+            }
+        });
+    });
+
+    observer.observe(box);
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const box = document.querySelector('.card1');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate__animated', 'animate__fadeInLeftBig');
+                entry.target.style.opacity = 1;
+                // Stop observing after the element is animated
+                observer.unobserve(entry.target);
+            }
+        });
+    });
+
+    observer.observe(box);
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const box = document.querySelector('.card2');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate__animated', 'animate__fadeInRightBig');
+                entry.target.style.opacity = 1;
+                // Stop observing after the element is animated
+                observer.unobserve(entry.target);
+            }
+        });
+    });
+
+    observer.observe(box);
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const box = document.querySelector('.projects-div');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate__animated', 'animate__fadeInRightBig');
+                entry.target.style.opacity = 1;
+                // Stop observing after the element is animated
+                observer.unobserve(entry.target);
+            }
+        });
+    });
+
+    observer.observe(box);
+});
+
